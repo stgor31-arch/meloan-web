24a7b3962a71a06d87b6ea61921720ad6907eb0c
stgor31-arch <stgor31@gmail.com>
2026-01-11 13:06:48 +0300

    Refactor main.tsx to use RootComponent

--- Meloan-PWA/client/src/main.tsx
+++ Meloan-PWA/client/src/main.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import Accept from "./pages/Accept";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Accept from './pages/Accept';
+import './index.css';
 
 const path = window.location.pathname;
+const RootComponent = path.startsWith('/accept') ? Accept : App;
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {path.startsWith("/accept") ? <Accept /> : <App />}
+    <RootComponent />
   </React.StrictMode>
 );
-
