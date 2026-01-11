7726df725eb7da1fc5914370de00d03805198a35
stgor31-arch <stgor31@gmail.com>
2026-01-11 10:16:20 +0300

    Add routing for Accept page in main.tsx

--- Meloan-PWA/client/src/main.tsx
+++ Meloan-PWA/client/src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import Accept from "./pages/Accept";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const path = window.location.pathname;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    {path.startsWith("/accept") ? <Accept /> : <App />}
+  </React.StrictMode>
+);
+
